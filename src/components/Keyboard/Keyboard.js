4c245d64a38203ace6a243aa9a7b876da3d57615
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-05-17 11:47:14 +0530

    App

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,41 +1,41 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { keys } from '../../constants/constants';
-import './Keyboard.css';
+import './Keyboard.css'
+
 
 const Keyboard = ({ boardData, handleKeyPress }) => {
-    const Keyboard = ({ boardData, handleKeyPress }) => {
 
-        function handleKeyboard(key) {
-            if (key.key === "Enter")
-                handleKeyPress("ENTER")
-            if (key.key === "Backspace")
-                handleKeyPress("⌫")
-            if (key.key.length === 1 && key.key.toLowerCase() !== key.key.toUpperCase())
-                handleKeyPress(key.key.toUpperCase())
-        }
-        useEffect(() => {
-            window.addEventListener("keydown", handleKeyboard)
+    function handleKeyboard(key) {
+        if (key.key === "Enter")
+            handleKeyPress("ENTER")
+        if (key.key === "Backspace")
+            handleKeyPress("⌫")
+        if (key.key.length === 1 && key.key.toLowerCase() !== key.key.toUpperCase())
+            handleKeyPress(key.key.toUpperCase())
+    }
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyboard)
 
-            return () => { window.removeEventListener("keydown", handleKeyboard) }
-        }, [handleKeyPress])
-        return (<div className='keyboard-rows' >
-            {keys.map((item, index) => (
-                <div className='row' key={index}>
-                    {
-                        item.map((key, keyIndex) => (
-                            <button key={keyIndex}
-                                className={`${boardData && boardData.correctCharArray.includes(key) ? "key-correct" :
-                                    (boardData && boardData.presentCharArray.includes(key) ? "key-present" :
-                                        boardData && boardData.absentCharArray.includes(key) ? "key-absent" : "")} `}
-                                onClick={() => { handleKeyPress(key) }}>
-                                {key}
-                            </button>
-                        ))
-                    }
-                </div>
-            ))}
+        return () => { window.removeEventListener("keydown", handleKeyboard) }
+    }, [handleKeyPress])
+    return (<div className='keyboard-rows' >
+        {keys.map((item, index) => (
+            <div className='row' key={index}>
+                {
+                    item.map((key, keyIndex) => (
+                        <button key={keyIndex}
+                            className={`${boardData && boardData.correctCharArray.includes(key) ? "key-correct" :
+                                (boardData && boardData.presentCharArray.includes(key) ? "key-present" :
+                                    boardData && boardData.absentCharArray.includes(key) ? "key-absent" : "")} `}
+                            onClick={() => { handleKeyPress(key) }}>
+                            {key}
+                        </button>
+                    ))
+                }
+            </div>
+        ))}
 
-        </div>);
-    };
-}
+    </div>);
+};
+
 export default Keyboard;
